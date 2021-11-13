6d65bf3d32af5b09a0900e421453211049814b45
ruslana <ruslana.voznyuk@gmail.com>
2021-11-13 13:46:24 +0200

    useNavigate() button back

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,15 +1,15 @@
 import React, { FC } from "react";
-import {} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 export const AboutPage: FC = () => {
-	
+	const history = useNavigate();
 	return(
 		<>
 			<h1>About Todo App</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam officia alias provident dolor ab laborum debitis a culpa asperiores saepe explicabo vel eius, molestiae sed aspernatur voluptatem maxime et ipsum.
 			</p>
-			<button className="btn">Back todos</button>
+			<button className="btn" onClick={() => history('/')}>Back todos</button>
 		</>
 	)
 }
